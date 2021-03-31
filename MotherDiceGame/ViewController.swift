6ceadff818fc7b5f555645bfadc7139547ef061b
Cem Olcay <ccemolcay@gmail.com>
2021-03-31 22:50:09 +0300

    Initial Commit

---
+++ MotherDiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotherDiceGame
+//
+//  Created by Cem Olcay on 3/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
